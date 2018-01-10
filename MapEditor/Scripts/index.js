8d25c8bf3009c34d189c02bbba11d71e1e37e9a2
Alejandro Olivera <aa.olivera@hotmail.com>
2018-01-10 04:22:35 -0300

    ajustes varios

--- MapEditor/Scripts/index.js
+++ MapEditor/Scripts/index.js
@@ -1,5 +1,4 @@
-﻿var ancho = 4;
-
+﻿var ancho = 1;
 $(document).ready(function () {
     var mapa = inicializarMapa();
     var canvas = document.getElementById('myCanvas');
@@ -11,7 +10,7 @@ $(document).ready(function () {
 
     canvas.onmousedown = function (e) {
         mouseIsDown = true;
-        drawCelda(ctx, e, mapa);
+        onclick(ctx, e, mapa);
     }
     canvas.onmouseup = function (e) {
         mouseIsDown = false;
@@ -20,11 +19,19 @@ $(document).ready(function () {
 
     canvas.onmousemove = function (e) {
         if (!mouseIsDown) return;
-        drawCelda(ctx, e, mapa);
+        onclick(ctx, e, mapa);
 
         return false;
     }
     document.addEventListener('contextmenu', event => event.preventDefault());
+
+    $("#guardar").click(function () {
+        guardar(mapa);
+    });
+
+    $("#abrir").click(function () {
+        document.getElementById('abrirOculto').click();
+    });
 });
 
 function inicializarMapa() {
@@ -45,19 +52,14 @@ function drawCanvas(ctx, mapa) {
     var imageObj = new Image();
 
     imageObj.onload = function () {
-        ctx.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height);
+        //Dibujo imagen
+        ctx.drawImage(imageObj, 0, 0, imageObj.width / 3, imageObj.height / 3);
 
+        //Dibujo los caminos
         for (var i = 0; i < mapa.length; i++) {
             var fila = mapa[i];
             for (var j = 0; j < fila.length; j++) {
-                ctx.lineWidth = 0.3;
-                if (fila[j] != 0) {
-                    primero = false;
-                    ctx.fillStyle = "blue";
-                    ctx.fillRect(j * ancho, i * ancho, ancho, ancho);
-                } else {
-                    ctx.rect(j * ancho, i * ancho, ancho, ancho);
-                }
+                drawCelda(ctx,j , i, fila[j]);
             }
         }
     };
@@ -65,26 +67,67 @@ function drawCanvas(ctx, mapa) {
     imageObj.src = 'http://localhost:56745/Img/Mapa.png';
 }
 
-function drawCelda(ctx, e, mapa) {
-    var x = e.offsetX - e.offsetX % ancho;
-    var y = e.offsetY - e.offsetY % ancho;
-    
-    if (event.which === 1) {
-        escribirEnConsola('Activa: ' + Math.round(x / 3) + '/' + Math.round(y / 3));
-        ctx.lineWidth = 0.3;
+function drawCelda(ctx, x , y, color) {
+    ctx.lineWidth = 0.3;
+    if (color == 1) {
         ctx.fillStyle = "blue";
         ctx.fillRect(x, y, ancho, ancho);
+    }
+    if (color == 3) {
+        ctx.fillStyle = "red";
+        ctx.fillRect(x, y, ancho, ancho);
+    }
+}
+
+function onclick(ctx, e, mapa) {
+    var x = Math.round((e.offsetX - e.offsetX % ancho ));
+    var y = Math.round((e.offsetY - e.offsetY % ancho ));
+
+    escribirEnConsola((event.which === 1 ? 'Activa: ' : 'Desactiva: ') + x + '/' + y);
+    drawCelda(ctx, x, y, event.which);
+
+    if (event.which === 1) {
         mapa[y][x] = 1;
     }
     else if (event.which === 3) {
-        escribirEnConsola('Desactiva: ' + Math.round(x / 3) + '/' + Math.round(y / 3));
-        ctx.lineWidth = 0.3;
-        ctx.fillStyle = "red";
-        ctx.fillRect(x, y, ancho, ancho);
         mapa[y][x] = 0;
     }
 }
 
 function escribirEnConsola(texto) {
     $("#consola").html(">" + texto + "<br />" + $("#consola").html());
+}
+
+function guardar(mapa) {
+    var texto = "";
+    var arrayLength = mapa.length;
+    for (var i = 0; i < arrayLength; i++) {
+        texto += mapa[i].join() + "\n";
+    }
+    var blob = new Blob([texto], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "mapa.txt");
+}
+
+function abrir(event) {
+    $.blockUI();
+    var input = event.target;
+    var resultado = "";
+    var reader = new FileReader();
+    reader.onload = function () {
+        resultado = reader.result;
+        console.log(reader.result.substring(0, 200));
+
+        var filas = resultado.split('\n');
+        var mapaAux = [];
+        var arrayLength = filas.length;
+        for (var i = 0; i < arrayLength; i++) {
+            mapaAux.push(filas[i].split(','));
+        }
+
+        var canvas = document.getElementById('myCanvas');
+        var ctx = canvas.getContext('2d');
+        drawCanvas(ctx, mapaAux);
+        $.unblockUI();
+    };
+    reader.readAsText(input.files[0]);    
 }
