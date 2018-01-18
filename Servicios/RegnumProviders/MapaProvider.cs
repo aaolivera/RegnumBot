add83afe225dc83a6f21eb27df9e322abd522442
aolivera <aa.olivera@hotmail.com>
2018-01-18 17:10:41 -0300

    Mover pj

--- Servicios/RegnumProviders/MapaProvider.cs
+++ Servicios/RegnumProviders/MapaProvider.cs
@@ -1,6 +1,7 @@
 ﻿using Dominio;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
@@ -10,11 +11,13 @@ namespace Servicios.RegnumProviders
     public class MapaProvider : RegnumProvider
     {
         private readonly CoordenadasProvider coordenadasProvider;
+        private readonly MoverPjProvider moverPjProvider;
         private readonly Mapa mapa;
 
-        public MapaProvider(CoordenadasProvider coordenadasProvider) : base(null, null)
+        public MapaProvider(CoordenadasProvider coordenadasProvider, MoverPjProvider moverPjProvider) : base(null, null)
         {
             this.coordenadasProvider = coordenadasProvider;
+            this.moverPjProvider = moverPjProvider;
             this.mapa = new Mapa();
 
             var _filePath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
@@ -35,6 +38,34 @@ namespace Servicios.RegnumProviders
         public void Mover(Point destino)
         {
             var coordenadaActual = coordenadasProvider.Obtener();
+        }
+
+        private void MoverANodo(Coordenada posActual, Nodo destino)
+        {
+            AlinearADestino(posActual, destino);
+            var distancia = destino.Distancia(posActual.Posicion);
+            moverPjProvider.Avanzar(distancia);
+        }
+
+        private void AlinearADestino(Coordenada posActual, Nodo destino)
+        {
+            var catetoOpuesto = Math.Abs(posActual.Posicion.Y - destino.Y);
+            var catetoAdyasente = Math.Abs(posActual.Posicion.X - destino.X);
+
+            var anguloEnRad = Convert.ToDecimal(Math.Atan(catetoOpuesto / catetoAdyasente) * Math.PI / 180);
+
+            if(posActual.Direccion != anguloEnRad)
+            {
+                var diferencia = posActual.Direccion - anguloEnRad;
+                if(diferencia <= 3)
+                {
+                    moverPjProvider.Girar(true, diferencia);
+                }
+                else
+                {
+                    moverPjProvider.Girar(false, diferencia);
+                }
+            }
         }
 
         public List<Nodo> DefinirCamino(Point desde, Point destino)
---
+++ Servicios/RegnumProviders/MoverPjProvider.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Servicios.RegnumProviders
+{
+    public class MoverPjProvider : RegnumProvider
+    {
+        public MoverPjProvider() : base(null, null)
+        {
+            
+        }
+
+        public void Girar(bool derecha, decimal rad)
+        {
+
+        }
+
+        public void Avanzar(double distancia)
+        {
+
+        }
+    }
+}
