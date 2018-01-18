f6588825027173c4bfcbff4f6920173a0aaf3a1f
aolivera <aa.olivera@hotmail.com>
2018-01-18 17:35:12 -0300

    subir tb

--- Servicios/RegnumProviders/MoverPjProvider.cs
+++ Servicios/RegnumProviders/MoverPjProvider.cs
@@ -1,21 +1,23 @@
 ﻿
+using Ninject.Extensions.Logging;
+
 namespace Servicios.RegnumProviders
 {
     public class MoverPjProvider : RegnumProvider
     {
-        public MoverPjProvider() : base(null, null)
+        public MoverPjProvider(ILogger log) : base(null, null, log)
         {
             
         }
 
         public void Girar(bool derecha, decimal rad)
         {
-
+            _log.Debug($"Girar direccion {derecha} cantidad {rad}");
         }
 
         public void Avanzar(double distancia)
         {
-
+            _log.Debug($"Avanzar {distancia}");
         }
     }
 }
